d3b2f28c62276501420615e2d9069022d0499248
AdonysProg <adonysprogramming@gmail.com>
2021-08-24 17:31:46 -0400

    chore: added test command

--- src/functions/georgiaScraper/index.ts
+++ src/functions/georgiaScraper/index.ts
@@ -1,5 +1,4 @@
-import schema from './schema';
-import { handlerPath } from '@libs/handlerResolver';
+import { handlerPath } from '@libs/handlerResolver'
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
@@ -8,12 +7,7 @@ export default {
       http: {
         method: 'get',
         path: 'georgiaScrapper/{drawDate}/{drawName}/{drawType}',
-        request: {
-          schema: {
-            'application/json': schema,
-          },
-        },
       },
     },
   ],
-};
+}
